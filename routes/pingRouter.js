c721f57ab5c109b1015e06f8f01363d1abbfc554
lightscrool <artem19o@yandex.ru>
2024-01-29 12:28:33 +0500

    Пофиксил TypeError

--- routes/pingRouter.js
+++ routes/pingRouter.js
@@ -2,7 +2,7 @@ const Router = require("express");
 const router = new Router();
 
 router.get("/", (req, res) => {
-  res.status(200).json({ message: "pong" });
+  return res.status(200).json({ message: "pong" });
 });
 
 module.exports = router;
